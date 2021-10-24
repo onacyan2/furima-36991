f2ecab85efb48dab9d28077f0ab8dc86e8bb1586
onacyan2 <omiz733NL@icloud.com>
2021-10-24 15:31:23 +0900

    新規登録の単体テストコード

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,18 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
-  validates :birth_date, presence: true
+  validates :nickname,              presence: true
+  validates :last_name,             presence: true
+  validates :last_name,             format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }, allow_blank: true
+  validates :first_name,            presence: true
+  validates :first_name,            format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }, allow_blank: true
+  validates :last_name_kana,        presence: true
+  validates :last_name_kana,        format: { with: /\A[ァ-ヶー－]+\z/ }, allow_blank: true
+  validates :first_name_kana,       presence: true
+  validates :first_name_kana,       format: { with: /\A[ァ-ヶー－]+\z/ }, allow_blank: true
+  validates :birth_date,            presence: true
+  validates :password,              format:{with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,}/}
+  validates :password_confirmation, format:{with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,}/}
 
   has_many :items
   has_many :orders
