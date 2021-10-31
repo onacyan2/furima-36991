18cff3b90eb43f2b2eb5abebb43facd6b64fec31
onacyan2 <omiz733NL@icloud.com>
2021-10-31 19:34:21 +0900

    クレジットカード決済の設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -8,6 +8,7 @@ class OrdersController < ApplicationController
     @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
+      pay_item
       @order_address.save
       redirect_to root_path
     else
@@ -19,7 +20,17 @@ class OrdersController < ApplicationController
 
   def order_params
     params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :addresses, :building, :phone_number).merge(
-      user_id: current_user.id, item_id: @item.id
+      user_id: current_user.id, item_id: @item.id, token: params[:token]
+    )
+  end
+
+  def pay_item
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+    binding.pry
+    Payjp::Charge.create(
+      amount: @item[:price],
+      card: order_params[:token],
+      currency: 'jpy'
     )
   end
 end
--- app/javascript/item.js
+++ app/javascript/item.js
@@ -1,9 +1,10 @@
 window.addEventListener('load', () => {
   const priceInput = document.getElementById("item-price");
+  if (!priceInput){ return false;}
     priceInput.addEventListener("input", () => {
       const addTaxDom = document.getElementById("add-tax-price");
       addTaxDom.innerHTML = Math.round(priceInput.value * 0.1 );
       const addPofitDom = document.getElementById("profit");
-      addPofitDom.innerHTML = Math.round(priceInput.value - Math.round(priceInput.value * 0.1 ))
-  })
+      addPofitDom.innerHTML = Math.round(priceInput.value - Math.round(priceInput.value * 0.1 ));
+    });
 });
