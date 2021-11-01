a996e4d116f5d08b16010b9fbe185f22e7b2d2f8
onacyan2 <omiz733NL@icloud.com>
2021-11-01 11:54:30 +0900

    商品購入機能の単体テストコード

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -2,8 +2,10 @@ class OrderAddress
   include ActiveModel::Model
   attr_accessor :postal_code, :prefecture_id, :city, :addresses, :building, :phone_number, :user_id, :item_id, :token
 
+  validates     :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+
   with_options presence: true do
-    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/ }
+    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :city
     validates :addresses
     validates :phone_number, format: { with: /\A[0-9]{11}\z/ }
